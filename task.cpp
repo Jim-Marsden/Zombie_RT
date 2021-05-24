efbedc1f8699096c78284aa0eae8983c091e1197
Jim Marsden <jamespmarsden@hotmail.com>
2021-05-23 22:16:42 -0700

    implmented tasks iterators

--- task.cpp
+++ task.cpp
@@ -3,3 +3,31 @@
 //
 
 #include "task.h"
+
+unsigned long zmb::task::size() {
+    return data.size();
+}
+
+std::vector<zmb::task::full_statement>::iterator zmb::task::begin() noexcept {
+    return data.begin();
+}
+
+std::vector<zmb::task::full_statement>::iterator zmb::task::end() noexcept {
+    return data.end();
+}
+
+std::vector<zmb::task::full_statement>::const_iterator zmb::task::cbegin() const noexcept {
+    return data.cbegin();
+}
+
+std::vector<zmb::task::full_statement>::const_iterator zmb::task::cend() const noexcept {
+    return data.cend();
+}
+
+std::vector<zmb::task::full_statement>::const_iterator zmb::task::begin() const noexcept {
+    return data.begin();
+}
+
+std::vector<zmb::task::full_statement>::const_iterator zmb::task::end() const noexcept {
+    return data.end();
+}
--- task.h
+++ task.h
@@ -9,15 +9,43 @@
 
 #include <vector>
 #include <string>
+#include <iostream>
+
 namespace zmb {
+
 
 
     class task {
         struct full_statement{dynamic_type type; task_statement_enum statement; };
-        std::string task_name;
         std::vector<full_statement> data;
+    public:
+        task() = default;
+        ~task() = default;
+        task(task const &) = default;
+        task(task &&) = default;
+        task &operator=(task const &) = default;
+
+        [[nodiscard]] decltype(data)::size_type size();
+
+        [[nodiscard]] decltype(data)::iterator begin() noexcept;
+        [[nodiscard]] decltype(data)::iterator end() noexcept;
+
+        [[nodiscard]] decltype(data)::const_iterator cbegin() const noexcept;
+        [[nodiscard]] decltype(data)::const_iterator cend() const noexcept;
+
+        [[nodiscard]] decltype(data)::const_iterator begin() const noexcept;
+        [[nodiscard]] decltype(data)::const_iterator end() const noexcept;
+
 
     };
+
+    template <class task_t, task_statement_enum statement_enum, class invokeable_t>
+    void invoke_task(task_t, invokeable_t invokeable){
+        if(statement_enum == task_statement_enum::test){
+            std::cout << "test!\n";
+        }
+
+    }
 }// namespace zmb
 
 #endif //ZOMBIE_RT_TASK_H
