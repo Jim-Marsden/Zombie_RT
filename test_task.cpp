2e98c13b711af930c93f04fa488704ef02cca364
Jim Marsden <jamespmarsden@hotmail.com>
2021-05-26 17:20:48 -0700

    added tasks tests

--- test_task.cpp
+++ test_task.cpp
@@ -3,14 +3,22 @@
 //
 
 #define CATCH_TEST_RUNNER
-
+#include <functional>
 #include <catch2/catch.hpp>
 #include "task.h"
 
-TEST_CASE("invoke_task", "default"){
+TEST_CASE("task::invoke_task", "default"){
     bool x{};
     zmb::task task;
     task.add_statement(zmb::dynamic_type{}, zmb::task_statement_enum::test);
-    zmb::invoke_task(task, [&x](auto a, auto b){x = true;});
+    std::function test = [&x](zmb::dynamic_type const &, zmb::task_statement_enum){ x = true;};
+    zmb::invoke_task(task, test);
     REQUIRE(x);
+}
+
+TEST_CASE("task::add_statement", "default"){
+    zmb::task task{};
+    REQUIRE(task.size() == 0);
+    task.add_statement({}, {});
+    REQUIRE(task.size() == 1);
 }
