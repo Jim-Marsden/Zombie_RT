53b518619b1eff4e562d49b0cf3269cab6e2143f
Jim Marsden <jamespmarsden@hotmail.com>
2021-05-30 22:09:05 -0700

    stub for entity.h

--- entity.h
+++ entity.h
@@ -5,9 +5,25 @@
 #ifndef ZOMBIE_RT_ENTITY_H
 #define ZOMBIE_RT_ENTITY_H
 
+#include "dynamic_type.h"
+#include "task.h"
+
 namespace zmb {
     class entity {
+        
+    public:
 
+        virtual void jump(zmb::task const & ztask, decltype(zmb::task::null_address) to_jump) = 0;
+        virtual void animate() = 0;
+        virtual void banish() = 0;
+        virtual void disturb(zmb::entity &) = 0;
+        virtual void forget() = 0;
+        virtual void invoke() = 0;
+        virtual void moan() = 0;
+        virtual void remember(/*zmb::entity_t::zombie*/) = 0;
+        virtual void remember(dynamic_type const & dynamic_type) = 0;
+
+        void say(dynamic_type const & dynamic_type);
     };
 }
 
