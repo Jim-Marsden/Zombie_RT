68cf52cd095654db7ba5465349ec179873059bf7
Jim <JamesPMarsden@hotmail.com>
2021-06-20 03:54:03 -0700

    Added all event functions as a virtual class. implemented most, remaining throws a std::runtime_error("not implemented")

--- entity.cpp
+++ entity.cpp
@@ -3,6 +3,8 @@
 //
 
 #include "entity.h"
+#include <exception>
+#include <iostream>
 
 zmb::task const & zmb::entity::back() const noexcept
 {
@@ -16,6 +18,7 @@ zmb::task& zmb::entity::back() noexcept
 
 void zmb::entity::add_task(const zmb::task& task_in) noexcept
 {
+    this->current_index = 0;
     tasks.push_back(task_in);
 }
 
@@ -27,4 +30,75 @@ decltype(zmb::entity::tasks.size()) zmb::entity::task_size() const noexcept
 bool zmb::entity::empty() const noexcept
 {
     return tasks.empty();
+}
+
+void zmb::entity::test() {
+    throw std::runtime_error("not implemented!");
+}
+
+void zmb::entity::noop() {}
+
+void zmb::entity::unknown() {
+    throw std::runtime_error("not implemented!");
+
+}
+
+void zmb::entity::jump(const zmb::dynamic_type &to_jump) {
+    tasks[current_index].set_stack_pointer(static_cast<zmb::task::address_size_t>(to_jump.get_as_numeric()));
+}
+
+void zmb::entity::loop(const zmb::dynamic_type &test_condition) {
+    throw std::runtime_error("not implemented!");
+}
+
+void zmb::entity::animate() {
+    throw std::runtime_error("not implemented!");
+}
+
+void zmb::entity::banish() {
+    throw std::runtime_error("not implemented!");
+}
+
+void zmb::entity::disturb(unsigned short index) {
+    throw std::runtime_error("not implemented!");
+}
+
+void zmb::entity::forget() {
+    memory.clear();
+
+}
+
+void zmb::entity::invoke() {
+    throw std::runtime_error("not implemented!");
+
+
+}
+
+zmb::dynamic_type zmb::entity::moan(zmb::dynamic_type const & data2) {
+    auto temp{memory};
+    memory = data2;
+    return temp;
+
+}
+
+void zmb::entity::remember(const zmb::dynamic_type &to_remeber) {
+    memory = to_remeber;
+
+
+}
+
+void zmb::entity::say(const zmb::dynamic_type &to_say) {
+    if(to_say.check_type_is_num()) std::cout << to_say.get_as_numeric();
+    else std::cout << to_say.get_as_string();
+    std::cout << '\n';
+
+    //std::cout << (to_say.check_type_is_num() ? to_say.get_as_numeric() : to_say.get_as_string()) << "\n";
+
+
+}
+
+bool zmb::entity::next_task() noexcept {
+    if(tasks.size() == (current_index + 1)) return false;
+    current_index++;
+    return true;
 }
--- entity.h
+++ entity.h
@@ -14,11 +14,21 @@ namespace zmb {
     class entity {
         dynamic_type memory;
 
-        std::vector <zmb::task> tasks;
+        std::vector<zmb::task> tasks;
 
-        
+        std::vector<zmb::task>::size_type current_index;
+
+
     public:
+        virtual ~entity() = default;
 
+        entity() = default;
+
+        entity(entity const &) = default;
+
+        entity(entity &&) = default;
+
+        entity& operator=(entity const &) = default;
 
 
         [[nodiscard]] decltype(tasks.size()) task_size() const noexcept;
@@ -26,13 +36,28 @@ namespace zmb {
         [[nodiscard]] bool empty() const noexcept;
 
 
-        void add_task(zmb::task const & task_in) noexcept;
+        void virtual add_task(zmb::task const &task_in) noexcept;
+        bool virtual next_task() noexcept; // true if it goes to next task
 
 
         //Will push_back if true;
-        [[nodiscard]] task const & back() const noexcept;
-        [[nodiscard]] task & back() noexcept;
+        [[nodiscard]] task const &back() const noexcept;
 
+        [[nodiscard]] task &back() noexcept;
+
+        virtual void test();
+        virtual void jump(zmb::dynamic_type const& to_jump);
+        virtual void loop(zmb::dynamic_type const& test_condition);
+        virtual void noop();
+        virtual void unknown();
+        virtual void animate();
+        virtual void banish();
+        virtual void disturb(unsigned short index);
+        virtual void forget();
+        virtual void invoke();
+        virtual zmb::dynamic_type moan(zmb::dynamic_type const & data2);
+        virtual void remember(zmb::dynamic_type const& to_remeber);
+        virtual void say(zmb::dynamic_type const& to_say);
 
     };
 }
